35066b4c98041bee4c7f229c9497c4a851979438
Karyn Black <caringkaryn5@gmail.com>
2025-09-16 01:49:19 -0400

    Problem with document.getElementById("bank")

--- homework-two/JavaScriptForSimpleCalculations.js
+++ homework-two/JavaScriptForSimpleCalculations.js
@@ -1,8 +1,7 @@
 // Clothes Shopping and Bank Account JS
-const priceShirt = 35
-const pricePants = 75
-const priceShoes = 60
+const priceShirt = 35;
+const pricePants = 75;
+const priceShoes = 60;
 
-let bankAccountBefore = 235.87
-let bankAccountAfter = bankAccountBefore - priceShirt - pricePants - priceShoes
-document.getElementById('bank').innerHTML = bankAccountAfter
+let bankAccountBefore = 235.87;
+let bankAccountAfter = bankAccountBefore - priceShirt - pricePants - priceShoes;
