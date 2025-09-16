b6bf15410a65efb671756d8e4126bf6c1e393f91
Karyn Black <caringkaryn5@gmail.com>
2025-09-16 01:16:28 -0400

    Clothes shopping and bank account JS variables and calculations created

---
+++ homework-two/JavaScriptForSimpleCalculations.js
@@ -0,0 +1,8 @@
+// Clothes Shopping and Bank Account JS
+const priceShirt = 35
+const pricePants = 75
+const priceShoes = 60
+
+let bankAccountBefore = 235.87
+let bankAccountAfter = bankAccountBefore - priceShirt - pricePants - priceShoes
+document.getElementById('bank').innerHTML = bankAccountAfter
