a53c1fb718c152c871bb69d76137eb3e594612bb
Karyn Black <caringkaryn5@gmail.com>
2025-09-16 15:33:05 -0400

    Done at the begging of class

--- homework-two/JavaScriptForSimpleCalculations.js
+++ homework-two/JavaScriptForSimpleCalculations.js
@@ -2,6 +2,9 @@
 const priceShirt = 35;
 const pricePants = 75;
 const priceShoes = 60;
+const priceJacket = 70;
 
 let bankAccountBefore = 235.87;
 let bankAccountAfter = bankAccountBefore - priceShirt - pricePants - priceShoes;
+
+let bankAccountLast = ((bankAccountAfter - priceJacket) >= 0);
--- inClass.js
+++ inClass.js
@@ -2,3 +2,5 @@ let nameNow = "Karyn";
 let age = 29;
 let email = "blackkar@idu.edu";
 console.log(nameNow + age + email);
+
+console.log("File");
