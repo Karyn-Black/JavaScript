94b7bb7d6d7de72eb393fe76462ca11bf0e91aca
Karyn Black <caringkaryn5@gmail.com>
2025-09-21 19:38:15 -0400

    Calculations AND 2 decimal places work

--- homework-three/interestCalculator.js
+++ homework-three/interestCalculator.js
@@ -1,16 +1,17 @@
 // Define the Variables
-let formulaPrinciple = 9000;
+let formulaPrinciple = 10000;
 let formulaRate = 2.56;
 let formulaRateNew = formulaRate / 100;
 let formulaTime = 8;
-let formulaTotalOwed;
-// formulaTotalOwed = formulaPrinciple(1+formulaRateNew*formulaTime);
+let formulaTotalOwed = formulaPrinciple * (1+formulaRateNew*formulaTime);
+formulaTotalOwed = formulaTotalOwed.toFixed(2);
 let formulaTotalInterest;
-// formulaTotalInterest = formulaTotalOwed-formulaPrinciple;
+formulaTotalInterest = formulaTotalOwed-formulaPrinciple;
+formulaTotalInterest = formulaTotalInterest.toFixed(2);
 
 // Enter Text into HTML
 document.getElementById("principle").innerText = "$" + formulaPrinciple;
 document.getElementById("rate").innerText = formulaRate + "%";
 document.getElementById("time").innerText = formulaTime + " years";
-// document.getElementById("interest").innerText = "$" + formulaTotalInterest;
-// document.getElementById("owed").innerText = "$" + formulaTotalOwed;
+document.getElementById("interest").innerText = "$" + formulaTotalInterest;
+document.getElementById("owed").innerText = "$" + formulaTotalOwed;
