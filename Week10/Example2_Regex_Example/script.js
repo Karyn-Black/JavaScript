843b09518035620d87072810aeafd7ede7d37549
Karyn Black <caringkaryn5@gmail.com>
2025-11-18 18:09:56 -0500

    Phone Number button works now

--- Week10/Example2_Regex_Example/script.js
+++ Week10/Example2_Regex_Example/script.js
@@ -3,6 +3,9 @@
 const input = document.querySelector('#userInput');
 const button = document.querySelector('#validateBtn');
 const resultDiv = document.querySelector('#result');
+const input2 = document.querySelector('#userInput2');
+const button2 = document.querySelector('#validateBtn2');
+const resultDiv2 = document.querySelector('#result2');
 
 button.addEventListener('click', () =>
     {
@@ -23,10 +26,11 @@ button.addEventListener('click', () =>
     }
 );
 
-button.addEventListener('click', () =>
+button2.addEventListener('click', () =>
     {
-        const userText2 = input.value;
-        const regex2 = /^\d{3}-\d{3}\d{4}$/;
+        const userText2 = input2.value;
+        // const regex2 = /^\d{3}-\d{3}\d{4}$/;
+        const regex2 = /^\d+$/;
         // ^ indicated the start of the sting
         // [A-Za-z] indicates any letter from A to Z or a to z
         // + indicates one or more occurrences of the preceding element
@@ -36,7 +40,7 @@ button.addEventListener('click', () =>
             resultDiv2.textContent = "Valid Number";
             resultDiv2.style.color = "green";
         }else{
-            resultDiv2.textContent = "Invalid Number (Only 9 numbers are allowed)";
+            resultDiv2.textContent = "Invalid Number (Only numbers are allowed)";
             resultDiv2.style.color = "red";
         }
     }
